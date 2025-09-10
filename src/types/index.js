9996341691d7de5375efd581e97c3cf1ff309442
mercadoalex <mercadoalex@gmail.com>
2025-09-09 21:01:29 -0600

    updating the graph

---
+++ src/types/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// src/types/index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+// This file can be expanded with more types and interfaces as the project grows.
+// For example, you might want to add types for error responses, configuration settings, or other data structures used in the application.
